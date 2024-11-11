58685df62848bd8946f394016abd24884014ca44
Isedin Karamehic <isedin_karamehic@teilnehmer.cdemy.de>
2024-11-11 15:37:11 +0100

    project started

---
+++ script.js
@@ -0,0 +1,7 @@
+let container = document.querySelector(".container");
+
+makeGrid(); //Calling function makeGrid
+
+function makeGrid() {
+	let text = ""; //defining of variable let, that has empty string value
+}
